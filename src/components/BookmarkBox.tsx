8e46a8eb667e3cd359f33459ea9cd6fde727be16
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2021-03-27 23:30:54 +0900

    コメント追加

--- src/components/BookmarkBox.tsx
+++ src/components/BookmarkBox.tsx
@@ -21,6 +21,7 @@ const Wrapper = styled.div`
   max-width: 450px;
   margin-left: 20px;
   margin-top: 10px;
+  /* semantic ui と同じスタイルにする */
   border-radius: .28571429rem;
   box-shadow: 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5;
   padding: 5px;
